dd780d4cc6d419ee9b2cbe4cfc2c1f182e21f40d
rongjun72 <rongjun72@github.com>
2026-08-21 09:50:20 +0800

    Adjust Nsym_total from 200*2000*50 to 200*2000*200

--- sim/smart_gmfsk_16x.m
+++ sim/smart_gmfsk_16x.m
@@ -27,7 +27,7 @@ timeBwProduct = 0.50;%0.50;  % Bandwidth-time product
 % timeBwProduct = BW*Tsym;   % Bandwidth-time product
 h = 0.5;                     % Modulation index, h = F_dev*Tsym = F_dev/SymbRate
 F_dev = h/Tsym;              % Max frequency deviation (Hz)
-Nsym_total = 200*2000*50; % Number of transmitted symbols
+Nsym_total = 200*2000*200; % Number of transmitted symbols
 Nsym_segment = 2000*40;
 EbNo_dB = 0 + 16*log10(1:1.9:20)/log10(20);
 f_off_ppm = 0;%0.5e-6;%6;%20ppm
